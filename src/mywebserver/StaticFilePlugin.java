c4fd95f1fbb10ad6a82f8c2a7b72f9f78e8e33bd
if13b067 <if13b067@technikum-wien.at>
2015-02-12 18:07:37 +0100

    U5 10/10 .. Static File Plugin geht

--- src/mywebserver/StaticFilePlugin.java
+++ src/mywebserver/StaticFilePlugin.java
@@ -1,26 +1,67 @@
 package mywebserver;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-import uebungen.UEB5Impl;
+
 import BIF.SWE1.interfaces.Plugin;
 import BIF.SWE1.interfaces.Request;
 import BIF.SWE1.interfaces.Response;
+import BIF.SWE1.interfaces.Url;
 
-public class StaticFilePlugin implements Plugin{
+public class StaticFilePlugin implements Plugin {
 
 	@Override
 	public float canHandle(Request req) {
-		if(new File(UEB5Impl.fileurl).exists() == true)						//siehe UEB5Impl
-			return 1;
-		
+
+		Url vUrl = req.getUrl();
+
+		// System.out.println("PFAD: " +
+		// System.getProperty("user.dir")+"\\"+vUrl.getPath());
+		// System.out.println("vURL: "+ vUrl.getPath());
+
+		if (new File(vUrl.getPath()).exists() == true) // siehe UEB5Impl
+			return 0.7f;
+
 		return 0;
 	}
 
 	@Override
 	public Response handle(Request req) {
+
+		ResponseImpl resp = new ResponseImpl();
+	
+		FileInputStream fs = null;
+		byte[] data = null;
+		 File file = new File(req.getUrl().getPath());
+
+
+		try {
+			try {
+				fs = new FileInputStream(file);
+				data = new byte[(int)file.length()];
+				fs.read(data); // Bytearray mit kodierten Daten
+			} finally {
+				if (fs != null)
+					fs.close();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		resp.setContent(data);
+	//	System.out.println("CONTENT: " + resp.content);
 		
-		return null;
+		
+		
+			
+		
+		
+		return resp;
 	}
 
 }
